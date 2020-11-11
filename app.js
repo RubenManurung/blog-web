94ad901d6756a33ad9aba765a4499e32d432914c
RubenManurung <rubenmichael199@gmail.com>
2020-11-11 13:57:18 +0700

    edit app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -8,7 +8,7 @@ const _ = require("lodash");
 const app = express();
 
 
-const homeStartingContent = "Selamat Datang di Marsiajar.id, temukan pengalaman belajar yang menyenangkan bersama dengan mentor dan pelajar lain. Disini kamu dapat menemukan materi pelajaran yang up to date dan sesuai dengan pasar teknologi diluar sana, jadi tunggu apalagi? MARI BELAJAR.";
+const homeStartingContent = "Selamat Datang di Marsiajar.id, temukan pengalaman belajar yang menyenangkan. Disini kamu dapat menemukan materi pelajaran yang up to date, jadi tunggu apalagi? MARI BELAJAR.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-ruben:mongodb2020@cluster0.jpgn1.mongodb.net/blogDB",{
+mongoose.connect(process.env.MONGO_URL,{
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
